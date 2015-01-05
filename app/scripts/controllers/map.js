199dadae4b64d8668dbacd3dc51836a6e7abdba2
Bjorn Gilstad <bjorn.gilstad@tihlde.org>
2015-01-05 14:43:11 +0100

    Update gitignore and delete stupid files.

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -8,21 +8,44 @@
  * Controller of the smartercarApp
  */
 angular.module('smartercarApp')
-  .controller('MapCtrl', function ($scope) {
+  .controller('MapCtrl', function ($scope, leafletData) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    angular.extend($scope, {
-      center: {
-        lat: 63.425,
-        lng: 10.397,
-        zoom: 14
 
-      },
-      defaults: {
-        scrollWheelZoom: false
-      }
-    });
+    var gpsConn = new WebSocket("ws://192.168.0.103:8080/gps");
+    var data;
+
+    gpsConn.onmessage = function(e) {
+        data = (JSON.parse(e.data));
+        console.log(data);
+          leafletData.getMap().then(function(map) {
+            map.panTo([62.784040,8.253784]);
+
+        });
+    };
+
+    $scope.center = {
+      lat: 63.425,
+      lng: 10.397,
+      zoom: 14
+
+    };
+    $scope.defaults = {
+      scrollWheelZoom: false
+    };
+
+//    angular.extend($scope, {
+//      center: {
+//        lat: 63.425,
+//        lng: 10.397,
+//        zoom: 14
+//
+//      },
+//      defaults: {
+//        scrollWheelZoom: false
+//      }
+//    });
   });
